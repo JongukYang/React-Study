f8874b53e28953ee1729e083a03332e7310a1a95
JongukYang <didwhddnr123@naver.com>
2022-10-20 21:56:29 +0900

    Update es6.js

--- react-frontend/base-js/es6.js
+++ react-frontend/base-js/es6.js
@@ -14,3 +14,29 @@ const tom = {
 
 const {name, age, height} = tom
 console.log(name, age, height)
+
+var lck = {
+    name: "lck",
+    print1: function() {
+        console.log(`[print1-1] name : ${this.name}`);
+        (function() {
+            console.log(`[print1-2] name : ${this.name}`);
+        })();
+    },
+    print2: function() {
+        console.log(`[print2-1] name : ${this.name}`);
+        var me = this;
+        (function() {
+            console.log(`[print2-2] name : ${this.name}`);
+        })();
+    },
+    print3: function() {
+        console.log(`[print3-1] name : ${this.name}`);
+        (() => {
+            console.log(`[print3-2] name : ${this.name}`);
+        })();
+    }
+};
+lck.print1();
+lck.print2();
+lck.print3();
