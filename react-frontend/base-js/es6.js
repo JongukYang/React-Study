7bcabe0f68164d49cfa1dd6a35352371f12f2caf
JongukYang <didwhddnr123@naver.com>
2022-10-20 21:50:32 +0900

    Update es6.js

--- react-frontend/base-js/es6.js
+++ react-frontend/base-js/es6.js
@@ -1,7 +1,16 @@
-console.log(name);
+// console.log(name);
 
-var name = "whddnr";
-const user = "whddnr";
+// var name = "whddnr";
+// const user = "whddnr";
 
-console.log(user);
-console.log(name);
+// console.log(user);
+// console.log(name);
+
+const tom = {
+    name: "tom",
+    age: 25,
+    height: 180
+}
+
+const {name, age, height} = tom
+console.log(name, age, height)
