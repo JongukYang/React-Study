3e9272477791dd5f09a3827fbd4282b2eef75cb5
JongukYang <didwhddnr123@naver.com>
2022-10-25 14:27:29 +0900

    Create es6-순수함수.js

---
+++ react-frontend/base-js/es6-순수함수.js
@@ -0,0 +1,5 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const number = numbers.reduce((acc, n) => acc + n, 0);
+console.log(number);
+const even_numbers = numbers.filter(i => i % 2 == 0);
+console.log(even_numbers);
